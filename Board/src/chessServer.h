24f1feb919b740a1e4280bb1b375ef85be562806
Austin Welch <66348244+AustinWelch@users.noreply.github.com>
2021-07-20 19:56:12 -0400

    Refactoring

--- Board/src/chessServer.h
+++ Board/src/chessServer.h
@@ -1,4 +1,4 @@
-#include <CC3100SupportPackage/sl_usage/sl_usage.h>
+#include "wifi_usage.h"
 #include "BSP.h"
 #include "debug.h"
 #include "fpu.h"
@@ -11,5 +11,23 @@
 #define WEBPAGE "3.95.241.253"
 #define BOARD_ID 0
 
-void chessServer_init();
-uint8_t chessServer_makeMove(char* gameCode, char* move);
+#define SUCCESS 0
+#define REQUEST_FAILED -1
+#define INVALID_RESPONSE -2
+#define WAITING -3
+#define INVALID_MOVE -4
+#define NOT_IN_GAME -5
+
+void chessServer_init(uint32_t conntype);
+int8_t chessServer_makeMove(char* move);
+int8_t chessServer_getLegalMoves(char* response);
+int8_t chessServer_newGame(char* response);
+int8_t chessServer_joinGame(char* response);
+int8_t chessServer_deleteGame(char* response);
+int8_t chessServer_awaitTurn();
+void chessServer_setGameCode(char* newGameCode);
+int8_t chessServer_getFriends(char* response);
+int8_t chessServer_addFriend(char* response, uint16_t friendId);
+int8_t chessServer_getCurrentGame();
+int8_t chessServer_setName(char* response, char* newName);
+uint8_t buildAndSendReq(char *parsedResponse);
